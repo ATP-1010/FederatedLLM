adad8b84ed7624486ecfbfd10794991004d1fecf
ATP-1010 <67300794+ATP-1010@users.noreply.github.com>
2024-04-27 18:11:28 -0400

    Update download.py

--- download.py
+++ download.py
@@ -1,4 +1,4 @@
 from huggingface_hub import snapshot_download
 
 snapshot_download(repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0", local_dir = 'tinyllama')
-snapshot_download(repo_id="huggyllama/llama-13b", local_dir = 'llama-13b')
+#snapshot_download(repo_id="huggyllama/llama-13b", local_dir = 'llama-13b')
