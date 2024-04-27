9c997557eb38d283e7fa2194ecd9829674459e6b
ATP-1010 <67300794+ATP-1010@users.noreply.github.com>
2024-04-27 18:10:25 -0400

    Update run_wiz.sh

--- run_wiz.sh
+++ run_wiz.sh
@@ -5,6 +5,9 @@ pip install -e .
 cd ../
 pip install huggingface_hub
 python download.py
+cd tinyllama
+wget https://huggingface.co/TinyLlama/TinyLlama-1.1B-Chat-v1.0/resolve/main/model.safetensors?download=true
+cd ../
 python main.py --global_model 'tinyllama' --data_path  "./data_wiz" --output_dir './nips-tinyllama-full-wiz-1-1-10/' --num_communication_rounds 1 --local_num_epochs 1 --full True
 python main.py --global_model 'llama-13b' --data_path  "./data_wiz" --output_dir './nips-llama13b-full-wiz-1-3-10/' --num_communication_rounds 1 --local_num_epochs 3 --full True
 python main.py --global_model 'llama-13b' --data_path  "./data_wiz" --output_dir './nips-llama13b-full-wiz-3-1-10/' --num_communication_rounds 3 --local_num_epochs 1 --full True
