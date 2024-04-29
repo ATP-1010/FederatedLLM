479fe367a4bb60ca5e5ee5eed37f19633f25f4d1
ATP-1010 <67300794+ATP-1010@users.noreply.github.com>
2024-04-28 23:51:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -242,6 +242,7 @@ def fl_finetune(
                 )
 
     if not ddp and torch.cuda.device_count() > 1:
+        print("Is it here??????")
         model.is_parallelizable = True
         model.model_parallel = True
 
