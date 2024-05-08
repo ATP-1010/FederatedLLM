ff2b8deaffe1a83f49ab3c5b77dec38633094b5b
ATP-1010 <67300794+ATP-1010@users.noreply.github.com>
2024-05-08 00:24:01 -0400

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,2 @@
+python main.py --global_model 'llama-13b' --data_path  "./data_mmlu" --output_dir './nips-llama13b-full-mmlu-1-3-10/' --num_communication_rounds 1 --local_num_epochs 3 --full True
+python main.py --global_model 'llama-13b' --data_path  "./data_mix" --output_dir './nips-llama13b-full-mix-1-3-20/' --num_communication_rounds 1 --local_num_epochs 3 --full True --num_clients 20
