779b03678566259ab3cb60cbaea266b7b2314353
ATP-1010 <67300794+ATP-1010@users.noreply.github.com>
2024-05-16 19:07:49 -0400

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,3 +1,7 @@
-python main.py --global_model 'llama-13b' --data_path  "./data_wiz" --output_dir './nips-llama13b-full-wiz-3-1-10/' --num_communication_rounds 3 --local_num_epochs 1 --full True
-#python main.py --global_model 'llama-13b' --data_path  "./data_mmlu" --output_dir './nips-llama13b-full-mmlu-3-1-10/' --num_communication_rounds 3 --local_num_epochs 1 --full True
-#python main.py --global_model 'llama-13b' --data_path  "./data_mix" --output_dir './nips-llama13b-full-mix-3-1-20/' --num_communication_rounds 3 --local_num_epochs 1 --full True --num_clients 20
+python main.py --global_model 'google/gemma-7b' --data_path  "./data_wiz" --output_dir './nips-gemma7b-full-wiz-1-3-10/' --num_communication_rounds 1 --local_num_epochs 3 --full True
+python main.py --global_model 'google/gemma-7b' --data_path  "./data_mmlu" --output_dir './nips-gemma7b-full-mmlu-1-3-10/' --num_communication_rounds 1 --local_num_epochs 3 --full True
+python main.py --global_model 'google/gemma-7b' --data_path  "./data_mix" --output_dir './nips-gemma7b-full-mix-1-3-10/' --num_communication_rounds 1 --local_num_epochs 3 --full True --num_clients 20
+
+python main.py --global_model 'google/gemma-7b' --data_path  "./data_wiz" --output_dir './nips-gemma7b-full-wiz-3-1-10/' --num_communication_rounds 3 --local_num_epochs 1 --full True
+python main.py --global_model 'google/gemma-7b' --data_path  "./data_mmlu" --output_dir './nips-gemma7b-full-mmlu-3-1-10/' --num_communication_rounds 3 --local_num_epochs 1 --full True
+python main.py --global_model 'google/gemma-7b' --data_path  "./data_mix" --output_dir './nips-gemma7b-full-mix-3-1-20/' --num_communication_rounds 3 --local_num_epochs 1 --full True --num_clients 20
